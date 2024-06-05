bcc7275cc62b49b3daa7d2f8f6e289e5b0ab15e8
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-06-04 23:53:00 -0500

    Fix page framing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Gift from "@/component/gift";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-start justify-between p-8">
       <div className="z-10 w-full max-w-5xl items-start justify-between font-mono text-sm lg:flex flex-col gap-8">
         <Gift itemName={"Diamond"} villagerNames={[V.Evelyn, V.Gus, V.Jodi, V.Krobus, V.Marnie, V.Maru, V.Penny, V.Willy]} />
         <Gift itemName={"Amethyst"} villagerNames={[V.Abigail, V.Clint, V.Dwarf, V.Emily]} />
