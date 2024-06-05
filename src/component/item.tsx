1efb3b22704f6e4b852c43f1aa17afda0ee33750
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-06-05 00:26:30 -0500

    Update bg color and opacity

--- src/component/item.tsx
+++ src/component/item.tsx
@@ -16,7 +16,7 @@ const Item = ({ itemName } : Props) => {
     }
     const itemImage = ITEM_IMAGES.filter((item) => item.name === itemName)[0].url
     return (
-        <Image className={`className="w-20 h-20"${isSelected ? ' hue-rotate-90' : ''} `} onClick={() => { toggleItem() }} src={itemImage} alt={""} width={50} height={50} />
+        <Image className={`w-20 h-20 ${isSelected ? ' hue-rotate-90' : ''} `} onClick={() => { toggleItem() }} src={itemImage} alt={""} width={50} height={50} />
     )
 }
 export default Item;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      boxShadow: {
+        glow: "0px 0px 18px .4px rgba(160, 221, 227, 0.1)",
+        fog: "0px 0px 18px .4px rgba(0, 0, 0, 0.1)",
       },
+      dropShadow: {
+        glow: [
+          "0 0px 5px #aad5b6",
+          "0 0px 65px rgba(255, 255, 255, 0.2)",
+          "0 0px 2px rgba(0, 0, 0, 1)"
+        ],
+        fog: [
+          "0 0px 10px #000000",
+          "0 0px 2px rgba(0, 0, 0, 1)"
+        ]
+      }
     },
   },
   plugins: [],
