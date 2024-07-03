17df77e8636e9925f9c365e67404d70d409482d9
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-07-02 23:47:08 -0500

    Try horizontal

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,11 @@ const config: Config = {
         ],
         fog: [
           "0 0px 10px #000000",
+          "0 0px 50px rgba(0, 0, 0, 1)",
+          "0 0px 5px rgba(0, 0, 0, 1)"
+        ],
+        fogger: [
+          "0 0px 10px #000000",
           "0 0px 2px rgba(0, 0, 0, 1)"
         ]
       }
